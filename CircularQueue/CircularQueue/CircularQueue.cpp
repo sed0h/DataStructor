02e26155360994f8172420747e730ac58d2c41b6
zgt <1352861058@qq.com>
2021-08-19 00:23:39 +0800

    add QUEUE_ELEMENT_TYPE and QUEUE_CURSOR_TYPE

--- CircularQueue/CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue/CircularQueue.cpp
@@ -1,12 +1,12 @@
 #include <stdint.h>
 #include "CircularQueue.h"
 
-void enqueue(circular_queue_t *q, uint8_t new_element) {
+void enqueue(circular_queue_t *q, QUEUE_ELEMENT_TYPE new_element) {
 	if (is_full(q)) {
 		return;
 	}
 	
-	uint8_t current_tail = q->_tail;
+	QUEUE_CURSOR_TYPE current_tail = q->_tail;
 	q->_elements[current_tail] = new_element;
 	q->_tail = next(current_tail, q);
 	q->_elements_count++;
@@ -18,17 +18,17 @@ void init(circular_queue_t *q) {
 	q->_elements_count = 0;
 }
 
-uint8_t get_size(circular_queue_t* q) {
+QUEUE_CURSOR_TYPE get_size(circular_queue_t* q) {
 	return q->_elements_count;
 }
 
 //check whether q is empty before call this fun
-uint8_t dequeue(circular_queue_t *q) {
+QUEUE_ELEMENT_TYPE dequeue(circular_queue_t *q) {
 	//if (is_empty(q)) {
 	//	return
 	//}
 
-	uint8_t current_head = q->_head;
+	QUEUE_CURSOR_TYPE current_head = q->_head;
 	q->_head = next(current_head, q);
 	q->_elements_count--;
 
@@ -37,7 +37,7 @@ uint8_t dequeue(circular_queue_t *q) {
 
 //queue index:0~QUEUE_SIZE-1
 bool is_full(circular_queue_t* q) {
-	uint8_t current_tail = q->_tail;
+	QUEUE_CURSOR_TYPE current_tail = q->_tail;
 	if (next(current_tail, q) == q->_head) {
 		return true;
 	}
@@ -54,7 +54,7 @@ bool is_empty(circular_queue_t *q) {
 	return (q->_head == q->_tail);
 }
 
-uint8_t next(uint8_t current_index, circular_queue_t *q) {
+QUEUE_CURSOR_TYPE next(QUEUE_CURSOR_TYPE current_index, circular_queue_t *q) {
 	if ((QUEUE_SIZE - 1) == current_index) {
 		return 0;
 	}
--- CircularQueue/CircularQueue/QueueConfig.h
+++ CircularQueue/CircularQueue/QueueConfig.h
@@ -1,2 +1,11 @@
-#define QUEUE_SIZE         (49)
-#define QUEUE_ELEMENT_TYPE (void*)
+#include <stdint.h>
+
+#define QUEUE_SIZE         (349)
+
+#if     (QUEUE_SIZE < (1<<8))
+#define QUEUE_CURSOR_TYPE uint8_t
+#elif   (QUEUE_SIZE < (1<<16))
+#define QUEUE_CURSOR_TYPE uint16_t
+#endif
+
+#define QUEUE_ELEMENT_TYPE uint8_t
